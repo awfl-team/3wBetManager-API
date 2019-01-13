03cb80a9d6cea01ec2c498ca07953b4bab8c403e
Florian <florob95@gmail.com>
2019-01-13 20:11:27 +0100

    add status to bet

--- Models/Bet.cs
+++ Models/Bet.cs
@@ -8,6 +8,7 @@ namespace Models
         public ObjectId Id { get; set; }
         public DateTime Date { get; set; }
         public int PointsWon { get; set; }
+        public string Status { get; set; }
         public User User { get; set; }
         public Match Match { get; set; }
     }
