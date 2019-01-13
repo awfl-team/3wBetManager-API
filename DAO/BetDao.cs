63db7491b4bc6b86262198b9576f0802310e377d
Florian <florob95@gmail.com>
2019-01-13 20:15:08 +0100

    change FindFinishBets function

--- DAO/BetDao.cs
+++ DAO/BetDao.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using DAO.Interfaces;
 using Models;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace DAO
 {
-    public class BetDao: IBetDao
+    public class BetDao : IBetDao
     {
         private readonly IMongoCollection<Bet> _collection;
 
@@ -14,6 +16,20 @@ namespace DAO
             var client = new MongoClient("mongodb://localhost:27017");
             var database = client.GetDatabase("3wBetManager");
             _collection = collection ?? database.GetCollection<Bet>("bet");
+        }
+
+        public async Task<List<Bet>> FindFinishBets(User user, int competitionId)
+        {
+            var betsByUser = await _collection.Find(bet => bet.User.Id == user.Id).ToListAsync();
+            foreach (var bet in betsByUser)
+            {
+                var matchInformation = await Singleton.Instance.MatchDao.FindMatch(bet.Match.Id);
+                bet.Match = matchInformation;
+            }
+            var betsByCompetition = betsByUser.FindAll(bet => bet.Match.Competition.Id == competitionId);
+            var betsByMatchStatus = betsByCompetition.FindAll(bet => bet.Match.Status == "FINISHED");
+
+            return betsByMatchStatus;
         }
 
         public async void AddBet(Bet bet)
--- DAO/Interfaces/IBetDao.cs
+++ DAO/Interfaces/IBetDao.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Models;
 
@@ -11,5 +8,6 @@ namespace DAO.Interfaces
     {
         void AddBet(Bet bet);
         void AddListBet(List<Bet> bets);
+        Task<List<Bet>> FindFinishBets(User user, int competitionId);
     }
 }
