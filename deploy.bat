d5e1f14ab87e76fe942ce622fe575f4797accb5b
Florian <florob95@gmail.com>
2019-03-06 23:06:46 +0100

    add -passThru test deplou n°4

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,4 @@
-powershell.exe Stop-Process -Name "3wBetManager-API" -ErrorAction silentlycontinue
+@ECHO OFF
+ECHO Start Deploy
+powershell.exe Stop-Process -Name "3wBetManager-API" -passThru -ErrorAction silentlycontinue
 start 3wBetManager-API\bin\Release\3wBetManager-API.exe
