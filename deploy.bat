fbbee579848c3afb07a0294e15c5700af806ca24
Florian <florob95@gmail.com>
2019-03-07 14:12:29 +0100

    test deploy n°11 exit in deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -2,3 +2,5 @@
 ECHO Start Deploy
 powershell.exe Stop-Process -Name "3wBetManager-API" -Force -ErrorAction silentlycontinue
 start 3wBetManager-API\bin\Release\3wBetManager-API.exe
+ECHO End Deploy
+exit 0
