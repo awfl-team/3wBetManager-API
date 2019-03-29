de4257e982d1555cf383359f097d37552e7354b8
Florian <florob95@gmail.com>
2019-03-29 14:28:59 +0100

    try to fix deploy gitlab ci bug

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 ECHO Start Deploy
 powershell.exe Stop-Process -Name "3wBetManager-API" -Force -ErrorAction silentlycontinue
-3wBetManager-API\bin\Release\3wBetManager-API.exe
+start 3wBetManager-API\bin\Release\3wBetManager-API.exe
 ECHO End Deploy
 exit 0
