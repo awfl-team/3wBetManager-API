2586a5446a5840016aba83050f2052b1880ec17c
Florian <florob95@gmail.com>
2019-03-29 13:59:10 +0100

    item dao

---
+++ DAO/Interfaces/IItemDao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DAO.Interfaces
+{
+    public interface IItemDao
+    {
+        Task<List<Item>> FindAllItems();
+        Task<Item> FindItem(string id);
+        Task UpdateCostItem(string id, Item item);
+    }
+}
