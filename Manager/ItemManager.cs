5608a12237802952ba855c44ff6bb5834546f510
Florian <florob95@gmail.com>
2019-03-29 13:58:41 +0100

    add item to user

---
+++ Manager/ItemManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DAO;
+using Models;
+
+namespace Manager
+{
+    public class ItemManager
+    {
+        public static async Task AddListItemsToUser(List<Item> items, User user)
+        {
+            var totalCost = 0;
+            foreach (var item in items)
+            {
+                await Singleton.Instance.UserDao.AddUserItem(item, user);
+                totalCost += item.Cost;
+            }
+
+            await Singleton.Instance.UserDao.UpdateUserPoints(user, (user.Point - totalCost),
+                user.TotalPointsUsedToBet);
+        }
+    }
+}
