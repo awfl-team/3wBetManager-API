f38c80cac3b34007599bf6a6be2e0cf84ffe337f
Florian <florob95@gmail.com>
2019-04-05 14:32:55 +0200

    fix bug

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO Start Deploy
-C:\Tools\BismiAppRunner\BismiAppRunner.exe START C:\GitLab-Runner\builds\Sns1WAaE\0\RomanLeichnig\backend-bet-manager-group2\3wBetManager-API\bin\Release\3wBetManager-API.exe
+C:\Tools\BismiAppRunner\BismiAppRunner.exe START C:\GitLab-Runner\builds\Sns1WAaE\0\RomanLeichnig\backend-bet-manager-group2\3wBetManager-API\bin\Release\\ 3wBetManager-API.exe
 ECHO End Deploy
 exit 0
