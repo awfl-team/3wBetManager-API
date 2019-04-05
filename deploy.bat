ce3856ad0fec86601e4d959a58e8870c99fd04ab
Florian <florob95@gmail.com>
2019-04-05 14:04:32 +0200

    test new srcipt

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
 ECHO Start Deploy
-powershell.exe Stop-Process -Name "3wBetManager-API" -Force -ErrorAction silentlycontinue
-start 3wBetManager-API\bin\Release\3wBetManager-API.exe
+C:\Tools\BismiAppRunner\BismiAppRunner.exe START 3wBetManager-API\bin\Release\3wBetManager-API.exe
 ECHO End Deploy
 exit 0
