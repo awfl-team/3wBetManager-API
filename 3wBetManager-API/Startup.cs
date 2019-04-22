6c973debdf881a7d06256283a245722a9e585a2f
Florian <florob95@gmail.com>
2019-04-22 17:45:01 +0200

    final version of notification hub

--- 3wBetManager-API/Startup.cs
+++ 3wBetManager-API/Startup.cs
@@ -13,11 +13,6 @@ namespace _3wBetManager_API
             var config = new HttpConfiguration();
             config.EnableSwagger(c => { c.SingleApiVersion("v1", "3wBetManager-API"); }).EnableSwaggerUi();
             config.MapHttpAttributeRoutes();
-            config.Routes.MapHttpRoute(
-                "3wBetManager-API",
-                "{controller}/{action}/{id}",
-                new {id = RouteParameter.Optional}
-            );
             appBuilder.UseCors(CorsOptions.AllowAll);
             appBuilder.UseWebApi(config);
             appBuilder.MapSignalR();
--- Hub/NotificationHub.cs
+++ Hub/NotificationHub.cs
@@ -1,19 +1,53 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Models;
 
 namespace Hub
 {
+    [HubName("notificationHub")]
     public class NotificationHub : Microsoft.AspNet.SignalR.Hub
     {
-        private static IHubContext hubContext = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+        private static readonly List<UserHub> ConnectedUsers =
+            new List<UserHub>();
 
-        public void Hello()
+        [HubMethodName("notification")]
+        public void SendNotification(string sentTo, string message)
         {
-            Clients.All.hello();
+            var user = ConnectedUsers.Find(u => u.Username == sentTo);
+            if (user == null) return;
+            var context = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+            context.Clients.Client(user.ConnectionId).NotifyUser(message);
         }
 
-        public static void SayHello()
+        public override Task OnConnected()
         {
-            hubContext.Clients.All.hello();
+            var connectionId = Context.ConnectionId;
+            var userName = Context.QueryString["username"];
+            var user = new UserHub{Username = userName, ConnectionId = connectionId};
+            var index = ConnectedUsers.FindIndex(u => u.ConnectionId == connectionId);
+            if (index == -1)
+            {
+                ConnectedUsers.Add(user);
+                return base.OnConnected();
+            }
+
+            ConnectedUsers[index] = user;
+            Console.WriteLine(userName);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+
+            var connectionId = Context.ConnectionId;
+            var index = ConnectedUsers.FindIndex(u => u.ConnectionId == connectionId);
+            ConnectedUsers.RemoveAt(index);
+
+            return base.OnDisconnected(stopCalled);
         }
     }
 }
