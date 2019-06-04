10a339ddddb26798e0b155f2b2497e7d54802c2c
lbourgeois <lucasbourgeois60@hotmail.fr>
2019-06-04 15:53:52 +0200

    Match & compet Manager

---
+++ Test/Manager/CompetitionManagerTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using DAO.Interfaces;
+using Manager;
+using Manager.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Test.Manager
+{
+    [TestFixture]
+    internal class CompetitionManagerTest
+    {
+        private ICompetitionDao _competitionDao;
+        private ICompetitionManager _competitionManager;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+           
+            _competitionDao = Substitute.For<ICompetitionDao>();
+            _competitionManager = SingletonManager.Instance.SetCompetitionManager(new CompetitionManager(_competitionDao));
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _competitionDao.ClearReceivedCalls();
+        }
+
+        [Test]
+        public async Task AssertThatGetAllCompetitionCallsFindAllCompetitions()
+        {
+            await _competitionManager.GetAllCompetition();
+            await _competitionDao.Received().FindAllCompetitions();
+        }
+    }
+}
---
+++ Test/Manager/MatchManagerTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DAO.Interfaces;
+using Manager;
+using Manager.Interfaces;
+using Models;
+using Newtonsoft.Json;
+using NSubstitute;
+using NUnit.Framework;
+using Test.Controller;
+
+namespace Test.Manager
+{
+    [TestFixture]
+    internal class MatchManagerTest
+    {
+        private IMatchDao _matchDao;
+        private IBetDao _betDao;
+        private IMatchManager _matchManager;
+        private static readonly List<Match> _matches = JsonConvert.DeserializeObject<List<Match>>(TestHelper.GetDbResponseByCollectionAndFileName("matches"));
+        private static readonly List<Bet> _bets = JsonConvert.DeserializeObject<List<Bet>>(TestHelper.GetDbResponseByCollectionAndFileName("bets"));
+        private Match _match = _matches[0];
+        private Bet _bet = _bets[0];
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+
+            _matchDao = Substitute.For<IMatchDao>();
+            _betDao = Substitute.For<IBetDao>();
+            _matchManager = SingletonManager.Instance.SetMatchManager(new MatchManager(_betDao, _matchDao));
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _matchDao.ClearReceivedCalls();
+            _betDao.ClearReceivedCalls();
+        }
+
+        [Test]
+        public async Task AssertThatCalculateMatchRatingCallsFindBetsByMatchAndUpdateMatches()
+        {
+            _betDao.FindBetsByMatch(_match).Returns(Task.FromResult(_bets));
+            _matchManager.CalculateMatchRating(_match);
+            await _betDao.Received().FindBetsByMatch(Arg.Any<Match>());
+            _matchDao.Received().UpdateMatch(Arg.Any<int>(), Arg.Any<Match>());
+        }
+    }
+}
